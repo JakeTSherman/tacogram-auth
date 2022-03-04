b4e1cec56e6cc9742631081b622f060185779923
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-03-04 04:50:59 +0000

    Step 1 Users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+      @user = User.where({email: params["email"]})[0]
+        if @user
+         if BCrypt::Password.new(@user.password) == params["password"]
+             session["user_id"] = @user.id
+
+             flash[:notice] = "Welcome! You logged in!"
+             redirect_to "/posts"
+         else
+             flash[:notice] = "Nope."
+             redirect_to "/sessions/new"
+         end
+        else
+         flash[:notice] = "Nope."
+         redirect_to "/sessions/new"
+     end
   end
 end
