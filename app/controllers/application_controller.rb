a1cf65e43bd32e1175078c8ebdd06ef76de077ed
JakeTSherman <jake.sherman@kellogg.northwestern.edu>
2022-03-04 05:44:39 +0000

    No posting unless logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
+    if session["user_id"]
+      @current_user = User.find(session["user_id"])
     puts "------------------ code before every request ------------------"
     # TODO: @current_user = ...
+    end
   end
 end
